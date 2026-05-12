ff89243fcea3deb0445f5c664605e0875d113c91
flintknock <flintknock@duck.com>
2026-05-12 00:35:40 -0400

    java script part2 (im goin 2 zzz)

--- assets/js/01-holamundo.js
+++ assets/js/01-holamundo.js
@@ -1,5 +1,22 @@
-alert("hola mundo");
-console.log("Estoy Aprendiendo JAVA SCRIPT");
-var nombre = "Marco"
-var edad = 20;
-console.log("Hola " + nombre + " y tienes " + edad + " años");
+/*alert("hola mundo");*/
+console.log("Estoy Aprendiendo JAVA SCRIPT")
+var nombre = 'Marco'
+var edad = 20
+console.log("Hola " + nombre + " y tienes " + edad + " años")
+
+let num1 = 15;
+let num2 = 25;
+let suma = num1 + num2;
+//forma antigua de imprimir en consola
+console.log("La suma de " + num1 + " mas " + num2 + " es igual a " + suma);
+//templates literales
+console.log(`La suma de ${num1} mas ${num2} es igual a ${suma}`);
+//otra manera
+console.log(`La suma de ${num1} mas ${num2} es igual a ${num1+num2}`);
+
+
+let ciudad="caracas"; //inicializar una variable
+console.log(ciudad);
+
+var ciudad2="maracay";
+console.log(ciudad2);
