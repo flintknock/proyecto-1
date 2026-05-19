e90ffe3f42b8200df27fee851a63e59ff8596fad
flintknock <flintknock@duck.com>
2026-05-19 14:55:35 -0400

    clase 3 javascript part1

---
+++ assets/js/06-while.js
@@ -0,0 +1,7 @@
+// WHILE = MIENTRAS SE EJECUTE LA CONDICION REALIZADA, LA INSTRUCCION LUEGO SE INCREMENTA
+
+let num = 5
+while(num<=15){
+    console.log(num);
+    num++
+}
---
+++ assets/js/07-function.js
@@ -0,0 +1,35 @@
+//function suma(n1,n2=3){
+    //conjunto de instrucciones que ejecuta la funcion
+    //console.log(`La suma de ${n1} mas ${n2} es igual a ${n1+n2}`);
+//}
+//suma(15,25) //invoca una funcion
+
+function porConsola(n1, n2){
+    /*parametros*/
+    console.log(`La suma es ${n1 + n2}`);
+    console.log(`La resta es ${n1 - n2}`);
+    console.log(`La multiplicacion es ${n1 * n2}`);
+    console.log(`La division es ${n1 / n2}`);
+    console.log(`***********************`);
+}
+porConsola(15,35)
+
+let suma ="";
+let multi ="";
+let resta ="";
+let divi ="";
+
+function porPantalla(n1, n2){
+    suma += `la suma de dos numeros es: ${n1 + n2}`;
+    document.getElementById("suma").innerHTML = suma;
+
+    multi += `la multiplicacion de dos numeros es: ${n1 * n2}`;
+    document.getElementById("multi").innerHTML = multi;
+
+   divi += `la division de dos numeros es: ${n1 / n2}`;
+    document.getElementById("divi").innerHTML = divi;
+
+    resta += `la resta de dos numeros es: ${n1 - n2}`;
+    document.getElementById("resta").innerHTML = resta;
+}
+porPantalla(60,15)
