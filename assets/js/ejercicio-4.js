c25d1b0ed524518c941918b4ac4151bc989b9e6d
flintknock <flintknock@duck.com>
2026-06-02 15:47:48 -0400

    clase 5 javascript parte 2

---
+++ assets/js/ejercicio-4.js
@@ -0,0 +1,25 @@
+//EJERCICIO 1
+//Una torilla de patatas lleva 200 gramos de patatas por persona
+//Por cada kilo de patatas se necesitan 5 huevos y 300 gramos de cebolla
+//Escribe un script que dado el numero de comensales calcula las cantidades de ingradientes necesarias
+
+let tortilla=200; //g de patatas
+let huevos=1;
+let cebolla=60; //g 
+
+let persona=5
+console.log(`si hay ${persona} comensales en el rest entonces ${tortilla*persona} g de patatas mas ${huevos*persona} huevos mas ${cebolla*persona} g de cebolla`);
+
+
+//EJERCICIO 2
+//ELABORA UN PROGRAMA QUE LE SOLICITE AL USUARIO LA CANTIDAD DE HORAS TRABAJADAS Y SU TRAFIA POR HORA
+//COMO RESULTADO DEBE MOSTRAR EL SALARIO TOTAL DEL TRABAJOR
+
+
+
+
+//EJERCICIO 3
+//ESCRIBE UN SCRIPT QUE PIDA AL USUARIO EL PRECIO ORIGINAL DE UN PRODUCTO
+//+ PORCENTAJE DE DESCUENTO
+//DEBE MOSTRAR EL MONTO A DESCONTAR
+//DEBE MOSTRAR EL PRECIO TOTAL MENOS EL DESCUENTO
